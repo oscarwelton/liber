d8d1c3054ecff350fd2bddcd2d8c8fef60df3b4a
oscar welton <oscarwelton@gmail.com>
2023-07-08 12:39:46 +0100

    updated to setup

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,18 @@ import helmet from "helmet";
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from "url";
+
+//  configurations
+
+dotenv.config();
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__fileName);
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helment.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")))
