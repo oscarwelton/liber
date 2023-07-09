0aed02053e205cba3e9dfe92b9373b1ba1ca2588
oscar welton <oscarwelton@gmail.com>
2023-07-09 14:44:57 +0100

    added get followers controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -9,3 +9,23 @@ export const getUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const getUserFollowers = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    const followers = await Promise.all(
+      user.followers.map((id) => User.findBy(id))
+    );
+    const formattedFollowers = followers.map(
+      ({ _id, firstName, lastName, bio, location, picturePath })
+    )
+    res.status(200).json(formattedFollowers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const addRemoveFollower = async (req, res) => {
+
+}
--- backend/models/User.js
+++ backend/models/User.js
@@ -29,7 +29,7 @@ const UserSchema = new mongoose.Schema (
       type: String,
       default: "",
     },
-    friends: {
+    followers: {
       type: Array,
       default: [],
     },
